e7901f4cd9bc28cfaa9630da3d59f51418b1e0cc
unnath123 <64188873+unnath123@users.noreply.github.com>
2023-08-28 15:30:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+
+
+
+const btn=document.getElementById("btn");
+
+async function check(inpt,dly){
+	let pr=await new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve(inpt);
+		},dly);
+	})
+
+    return pr;
+}
+
+//btn.addEventListener("click",check1//);
+
+function check1(){
+    let inputvalue=document.getElementById("text").value;
+    let delayvalue=document.getElementById("delay").value;
+
+    check(inputvalue,delayvalue).then((data)=>{
+        console.log(data)
+        document.getElementById("output").innerText=data;
+    })
+}
+
+
+
